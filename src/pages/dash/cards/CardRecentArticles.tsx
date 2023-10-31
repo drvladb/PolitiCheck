df1a3861111ae0709386b59a6a386ff60a9fe1e1
Vlad Bondar <vlad@vladb.xyz>
2023-10-31 13:07:02 -0700

    reverse article order in dash

--- src/pages/dash/cards/CardRecentArticles.tsx
+++ src/pages/dash/cards/CardRecentArticles.tsx
@@ -39,7 +39,7 @@ export default function CardPageVisits() {
             bias: d.bias
           })
         })
-        setArticles(iArticles)
+        setArticles(iArticles.reverse()) // reverse to get chronological order
       });
     })
   }, [])
@@ -55,12 +55,12 @@ export default function CardPageVisits() {
               </h3>
             </div>
             <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-              <button
+              {/* <button
                 className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
               >
                 See all
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
